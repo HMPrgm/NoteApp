7ffe06c157800b60003d38c25ea5b5e65a312f45
MSI Laptop <henrymunprof@gmail.com>
2024-05-17 16:22:37 -0400

    Changed interface so now notes are all displayed on one screen. Added note saving through use of a csv file. changed some colors as well. Known Bugs - New lines don't save :(

--- Notes/MainWindow.xaml.cs
+++ Notes/MainWindow.xaml.cs
@@ -19,46 +19,110 @@ namespace Notes
     public partial class MainWindow : Window
     {
         private int score = 0;
+        private Note curNote;
+        
         public MainWindow()
         {
             InitializeComponent();
+            Note.InitializeNotes();
+            RefreshApp();
         }
 
-        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        private void RefreshApp(Note? note = null)
         {
-            Note.Add(noteBox.Text, noteTitle.Text);
-            UpdateNoteList();
-            noteBox.Clear(); noteTitle.Clear();
+            if (note == null) { note = Note.GetFirst(); }
+            RefreshNoteList();
+            UpdateCurrentNote(note);
         }
 
-        private void UpdateNoteList()
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveCurrentNote();
+            Note.SaveNotes();
+        }
+
+
+
+        private void noteList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //Code that gets item clicked (I don't really know how it works)
+            DependencyObject dep = (DependencyObject)e.OriginalSource;
+            while ((dep != null) && !(dep is ListViewItem))
+            {
+                dep = VisualTreeHelper.GetParent(dep);
+            }
+            if (dep == null){ return; }
+            TextBlock item = (TextBlock)noteList.ItemContainerGenerator.ItemFromContainer(dep);
+            UpdateCurrentNote(Note.FindByID((int)item.Tag));
+        }
+
+        private void RefreshNoteList()
         {
             noteList.Items.Clear();
             foreach (Note note in Note.Get())
             {
                 TextBlock text = new TextBlock();
-                text.Text = note.getTitle();
+                text.Text = note.getTitle() == "" ? "Untitled Note" : note.getTitle();
                 text.Name = "Note_" + note.getId().ToString();
                 text.Tag = note.getId();
                 noteList.Items.Add(text);
             }
         }
 
-        private void noteList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void UpdateCurrentNote(Note? note)
         {
-            DependencyObject dep = (DependencyObject)e.OriginalSource;
-
-            while ((dep != null) && !(dep is ListViewItem))
-            {
-                dep = VisualTreeHelper.GetParent(dep);
-            }
-
-            if (dep == null)
-                return;
-
-            TextBlock item = (TextBlock)noteList.ItemContainerGenerator.ItemFromContainer(dep);
-            ShowNote noteView = new ShowNote((int)item.Tag);
-            noteView.Show();
+            if (note == null) { return; }
+            //Saves old note
+            if (curNote != null) { SaveCurrentNote(); }
+            //Sets New Note
+            curNote = note;
+            noteTitle.Text = note.getTitle();
+            noteBox.Text = note.getText();
         }
+
+        private void SaveCurrentNote()
+        {
+            if (curNote == null) { return; }
+            curNote.setText(noteBox.Text);
+            curNote.setTitle(noteTitle.Text);
+            RefreshNoteList();
+        }
+
+        private void NoteChanged(object sender, TextChangedEventArgs e)
+        {
+            mainWindow.Title = noteTitle.Text.Length > 0 ? noteTitle.Text : "Untitled Note";
+            
+        }
+
+        private void AddNewNote(object sender, RoutedEventArgs e) 
+        {
+            Note note = AddNewNote();
+            RefreshApp(note);
+        }
+        private Note AddNewNote(string title = "", string text = "")
+        {
+            Note note = new Note(title: title, text: text);
+            Note.Add(note);
+            return note;
+        }
+
+        private void DeleteCurrentNote(object sender, RoutedEventArgs e)
+        {
+            if (curNote != null)
+            { 
+                Note.Remove(curNote); 
+            }
+            if (Note.GetFirst() == null)
+            {
+                AddNewNote();
+            }
+            RefreshApp();
+        }
+
+        
     }
 }
--- Notes/Note.cs
+++ Notes/Note.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace Notes
 {
     public class Note
     {
-        private String text;
-        private String title;
+        private string text;
+        private string title;
         private int id;
         private static int CURR_ID = 1;
 
-        public Note(String text, String title)
+        public Note(string text, string title)
         {
             this.text = text;
             this.title = title;
@@ -21,21 +21,22 @@ namespace Notes
             this.id = CURR_ID;
             CURR_ID++;
         }
+
         #region Accessors/Mutators
-        public String getText()
+        public string getText()
         {
             return text;
         }
-        public void setText(String text)
+        public void setText(string text)
         {
             this.text = text;
         }
 
-        public String getTitle()
+        public string getTitle()
         {
             return title;
         }
-        public void setTitle(String title)
+        public void setTitle(string title)
         {
             this.title = title;
         }
@@ -44,21 +45,51 @@ namespace Notes
         {
             return id;
         }
-        #endregion
-        //STATIC
-        private static List<Note> notes = new List<Note>();
-
-        public static bool Remove(int _id)
+        public string ToCSV()
         {
-            for (int i = 0; i < notes.Count; i++)
-            {
-                if (_id==notes.ElementAt(i).id)
+            return $"{this.id},{this.title},{this.text}";
+        }
+        #endregion
+        /********** NOTE STORAGE **********
+         * Notes are stored in a csv file "notes.csv"
+         * The csv order is [id,title,text]
+         * Ids are overridden each time
+         */
+        private static List<Note> notes = new List<Note>();
+        private static string filename = "notes.csv";
+        private static int ITEMS_VALIDATION =3;
+        
+        public static void InitializeNotes()
+        {
+            try {
+                List<string> contents = File.ReadAllLines(filename).ToList();
+                foreach (string line in contents)
                 {
-                    notes.RemoveAt(i);
-                    return true;
+                    if (line.Count(f => f == ',') != ITEMS_VALIDATION - 1) { continue; }
+
+                    string[] lsplit = line.Split(",");
+                    notes.Add(new Note(lsplit[2], lsplit[1]));
                 }
             }
-            return false;
+            catch (FileNotFoundException)
+            {
+                File.Create(filename);
+            }
+        }
+        public static void SaveNotes()
+        {
+            //"{note.id},{note.title},{note.text}";
+            string[] contents = new string[notes.Count()];
+            for (int i = 0; i < notes.Count(); i++)
+            {
+                contents[i] = notes[i].ToCSV();
+            }
+            File.WriteAllLines(filename, contents);
+
+        }
+        public static bool Remove(Note note)
+        {
+            return notes.Remove(note);
         }
 
         public static void Add(Note note)
@@ -66,14 +97,18 @@ namespace Notes
             notes.Add(note);
         }
 
-        public static void Add(String text, String title)
+        public static void Add(string text, string title)
         {
-            notes.Add(new Note(text, title));
+            Add(new Note(text, title));
         }
 
         public static List<Note> Get()
         {
             return notes;
+        }
+        public static Note? GetFirst()
+        {
+            return notes.Count() > 0? notes[0]:null;
         }
         public static Note? FindByID(int id)
         {
@@ -82,6 +117,16 @@ namespace Notes
                 if (note.id == id) return note;
             }
             return null;
+        }
+
+        public static bool Update(int id, string title = "", string text = "")
+        {
+            Note? note = FindByID(id);
+            if (note == null) return false;
+            if (title != "") { note.title = title; }
+            if (text != "") { note.text = text; }
+
+            return true;
         }
 
     }
